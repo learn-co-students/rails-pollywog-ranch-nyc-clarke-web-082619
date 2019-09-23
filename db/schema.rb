2cf7b2e71d96adfc049b058c9a6a42db0c0655f1
github username <github email address>
2019-09-23 15:16:44 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -18,12 +18,13 @@ ActiveRecord::Schema.define(version: 20140922151507) do
     t.integer "pond_id"
   end
 
-  create_table "ponds", force: :cascade do |t|
-    t.string "water_type"
+  create_table "pond", force: :cascade do |t|
     t.string "name"
+    t.string "water_type"
+    t.string "color"
   end
 
-  create_table "tadpoles", force: :cascade do |t|
+  create_table "tadpole", force: :cascade do |t|
     t.string  "name"
     t.string  "color"
     t.integer "frog_id"
